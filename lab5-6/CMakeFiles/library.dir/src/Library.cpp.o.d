27ad538016e4a69228004625e720d4dfd4c69064
kernel_paniccc <rtyrtyrtybbb@gmail.com>
2025-12-12 01:46:44 +0300

    stupid file using

--- lab5-6/CMakeFiles/library.dir/src/Library.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/library.dir/src/Library.cpp.o: \
- /home/kali/Projects-git/MIEM_C_plus_plus/lab5-6/src/Library.cpp \
- /usr/include/stdc-predef.h
--- lab5-6/src/Library.cpp
+++ lab5-6/src/Library.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 
 
 using namespace std;
@@ -64,9 +65,94 @@ void Library::displayAllUsers() {
 }
 
 void Library::saveToFile() {
+  ofstream out(dataFile);
+  if (!out.is_open()) {
+    return;
+  }
 
+  out << books.size() << '\n';
+  for (const auto& book : books) {
+    out << book.getTitle() << '|'
+        << book.getAuthor() << '|'
+        << book.getYear() << '|'
+        << book.getIsbn() << '|'
+        << book.getIsAvailable() << '|'
+        << book.getBorrowedBy() << '\n';
+  }
+
+  out << users.size() << '\n';
+  for (const auto& user : users) {
+    const auto borrowed = user.getBorrowedBooks();
+    out << user.getName() << '|'
+        << user.getUserId() << '|'
+        << user.getMaxBooksAllowed() << '|';
+    for (size_t i = 0; i < borrowed.size(); ++i) {
+      if (i) {
+        out << ',';
+      }
+      out << borrowed[i];
+    }
+    out << '\n';
+  }
 }
 
 void Library::loadFromFile() {
- 
+  ifstream in(dataFile);
+  if (!in.is_open()) {
+    return;
+  }
+
+  books.clear();
+  users.clear();
+
+  string line;
+  size_t booksCount = 0;
+  if (!(in >> booksCount)) {
+    return;
+  }
+  getline(in, line);  // consume endline
+
+  for (size_t i = 0; i < booksCount && getline(in, line); ++i) {
+    istringstream ss(line);
+    string title, author, yearStr, isbn, availableStr, borrowedBy;
+    getline(ss, title, '|');
+    getline(ss, author, '|');
+    getline(ss, yearStr, '|');
+    getline(ss, isbn, '|');
+    getline(ss, availableStr, '|');
+    getline(ss, borrowedBy, '|');
+
+    Book book(title, author, stoi(yearStr), isbn);
+    const bool available = availableStr != "0";
+    if (!available) {
+      book.borrowBook(borrowedBy);
+    }
+    books.push_back(book);
+  }
+
+  size_t usersCount = 0;
+  if (!(in >> usersCount)) {
+    return;
+  }
+  getline(in, line);  // consume endline
+
+  for (size_t i = 0; i < usersCount && getline(in, line); ++i) {
+    istringstream ss(line);
+    string name, userId, maxBooksStr, borrowedStr;
+    getline(ss, name, '|');
+    getline(ss, userId, '|');
+    getline(ss, maxBooksStr, '|');
+    getline(ss, borrowedStr, '|');
+
+    vector<string> borrowedBooks;
+    istringstream borrowedStream(borrowedStr);
+    string isbn;
+    while (getline(borrowedStream, isbn, ',')) {
+      if (!isbn.empty()) {
+        borrowedBooks.push_back(isbn);
+      }
+    }
+
+    users.emplace_back(name, userId, borrowedBooks, stoi(maxBooksStr));
+  }
 }
